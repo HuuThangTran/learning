34ebd24d52be2cea2b9cc7fed7891e4f5c7e70f3
mle288 <thanghuu.work@gmail.com>
2024-01-15 10:10:28 -0500

    js

---
+++ brock/2023winterquarter/COSC_1P02/week1/polygon.js
@@ -0,0 +1,16 @@
+import Media.*;
+import static java.lengh.Math.*;
+
+public class polygon {
+    public int nVertices;
+    // public int n = 1.5; return an error
+    // if you declare anything safe to save public
+
+    public polygon(){ //constructor of the class
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
