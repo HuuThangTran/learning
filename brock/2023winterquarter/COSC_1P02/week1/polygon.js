3d91b211442ac92538dcfa3963edc2a5c4d60efa
mle288 <thanghuu.work@gmail.com>
2024-01-15 10:38:33 -0500

    js week1

--- brock/2023winterquarter/COSC_1P02/week1/polygon.js
+++ brock/2023winterquarter/COSC_1P02/week1/polygon.js
@@ -5,12 +5,35 @@ public class polygon {
     public int nVertices;
     // public int n = 1.5; return an error
     // if you declare anything safe to save public
+    public Turtle yertle;
+    public TurtleDisplayer display;
 
     public polygon(){ //constructor of the class
+        yertle = new Turtle(); // initiallization
+        display = new this.TurtleDisplayer();
+        
+        display.placeTurtle(yertle);
+        yertle.penDown();
 
+
+
+
+        yertle.forward(40);
+        yertle.right(PI/2);
+        yertle.forward(40);
+        yertle.right(PI/2);
+        yertle.forward(40);
+        yertle.right(PI/2);
+        yertle.forward(40);
+        yertle.right(PI/2);
+
+        // check method by turtle brock library
+        // class turtle and class tutle displayer
+        // brock package media
+        // installing bluej
     }
 
     public static void main(String[] args) {
-        
+
     }
 }
