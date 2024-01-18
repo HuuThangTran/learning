7fd469298fd4c8887ba794a9fd37965457de32e0
mle288 <thanghuu.work@gmail.com>
2024-01-18 17:30:54 -0500

    creating file pa1 ucsc algorithm and brock js

--- brock/2023winterquarter/COSC_1P02/week1/polygon.js
+++ brock/2023winterquarter/COSC_1P02/week1/polygon.js
@@ -34,6 +34,39 @@ public class polygon {
     }
 
     public static void main(String[] args) {
-
+        // create an instance of polygon class
+        Polygon sq = new Polygon;
     }
+
+
+
+    //loop
+    //for statements
+    // for loop for turtle pen square
+    for (int i = 1; i <= 4 ; i++) {
+        yertle.forward(40);
+        yertle.right(Math.PI/2);
+    }
+    
+    // for loop for turtle pen hexagon
+    for (int = 0; i <6; i ++) {
+        yertle.forward(40);
+        yertle.right(Math.PI/3);
+        //yertle.right(PI/3)
+    }
+
+    //nesting = one loop in another loop (composition)
+    for (int j = 0; j < 6; j++) {
+        for (int i = 0; i < 6; i ++){
+            yertle.forward(40);
+            yertle.right(PI/3);
+        }
+        yertle.forward(40);
+        yertle.left(PI/3);
+    }
+    // object and variables
+    //object is an instance of a class, kind of variable
+    //statment: end with ;
+    // calling a method object.methodname;
+    
 }
